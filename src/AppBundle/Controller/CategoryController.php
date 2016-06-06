46d24962e43bc8ea96fce9a943696322eff00d76
sydorenkovd <sydorenkovd@gmail.com>
2016-06-06 12:46:21 +0300

    create

--- src/AppBundle/Controller/CategoryController.php
+++ src/AppBundle/Controller/CategoryController.php
@@ -5,6 +5,10 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\Category;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Tests\Extension\Core\Type\SubmitTypeTest;
 use Symfony\Component\HttpFoundation\Request;
 
 class CategoryController extends Controller
@@ -28,8 +32,33 @@ $categories = $this->getDoctrine()
     public function createAction(Request $request)
     {
 
+        $category = new Category();
+        $form = $this->createFormBuilder($category)
+            ->add('name', TextType::class,
+                ['attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px']])
+            ->add('save', SubmitType::class,
+                ['label' => 'Create Category', 'attr' => ['class' => 'btn btn-primary']])
+            ->getForm();
+        //handle request
+        $form->handleRequest($request);
+        //check submit
+        if($form->isSubmitted() && $form->isValid()){
+            $name = $form['name']->getData();
+            //Get current date time
+            $now = new \DateTime('now');
+            $category->setName($name);
+            $category->setCreateDate($now);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($category);
+            $em->flush();
+            $this->addFlash(
+                'notice',
+                'Category Saved'
+            );
+            return $this->redirect('/category');
+        }
         return $this->render('category/create.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+            'form' => $form->createView()
         ]);
     }
     /**
